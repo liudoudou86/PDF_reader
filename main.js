e8263fefe52cf195f7f70c2ea818504c24554fbb
lz <543972930@qq.com>
2021-08-23 09:57:09 +0800

    update coding

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
-const {app, BrowserWindow, Menu, Tray} = require('electron')
+const { app, BrowserWindow, dialog, Menu, Tray, globalShortcut } = require('electron')
 const path = require('path')
+const PDFWindow = require('electron-pdf-window')
 
-var tray = null
+let tray = null
 
-function createWindow () {
+app.whenReady().then(() => {
   /* 创建项目窗口 */
   const mainWindow = new BrowserWindow({
-    width: 500,
-    height: 200,
+    frame: false,
     webPreferences: {
       preload: path.join(__dirname, './src/preload.js')
     }
   })
 
-  /* 指向html页面进行渲染 */
-  mainWindow.loadFile('./src/index.html')
-  /* 修改应用菜单 
-  const menu = Menu.buildFromTemplate([])
-  Menu.setApplicationMenu(menu) */
+  /* 自动隐藏 */
+  mainWindow.hide()
+
   /* 创建系统托盘 */
-  tray = new Tray('./pubilc/e6.ico')
+  tray = new Tray(path.join(__dirname, './image/e6.ico')) // 此写法为全局变量方便打包后使用
   const contextMenu = Menu.buildFromTemplate([
-    {
-      label: '显示', role: 'show', click: () => {
-        mainWindow.show()
-      }
-    },
     {
       label: '隐藏', role: 'hide', click: () => {
         mainWindow.hide()
@@ -35,16 +28,37 @@ function createWindow () {
       label: '退出', role: 'quit', click: () => {
         app.quit()
       }
-    }
+    } 
   ])
   tray.setToolTip(app.name)
   tray.on('right-click', () => {
     tray.popUpContextMenu(contextMenu)
   })
-}
 
-app.whenReady().then(() => {
-  createWindow()
+  /* 创建快捷键 */
+  globalShortcut.register('CommandOrControl+Space', () => {
+
+    mainWindow.show()
+    mainWindow.maximize()
+
+    /* 创建浏览文件夹 */
+    dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+        filters: [
+            { name : 'All Files', extensions : ['*'] }
+        ]
+    }).then(result => {
+        if (result.canceled) {
+            console.log('User manually canceled')
+        } else {
+            const filepath = result.filePaths[0]
+            mainWindow.loadURL(`file://${filepath}`) //该位置使用Tab键上面的符号
+            PDFWindow.addSupport(mainWindow)
+        }
+    }).catch(err => {
+        console.log(err)
+    })
+  })
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
