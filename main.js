3f5849fc00f9c3b33f92272a59da223c6b45526f
lz <543972930@qq.com>
2021-09-10 16:24:06 +0800

    update coding

--- main.js
+++ main.js
@@ -9,26 +9,36 @@ app.whenReady().then(() => {
   /* 创建项目窗口 */
   const mainWindow = new BrowserWindow({
     frame: false,
-    webPreferences: {
-      preload: path.join(__dirname, './src/preload.js')
-    }
+    icon : "./image/256x256.ico",
+    width : 700,
+    height : 65,
+    backgroundColor : '#000000'
   })
 
   /* 自动隐藏 */
   mainWindow.hide()
 
+  mainWindow.loadFile("./src/index.html")
+
+  /* 定义窗口位置 */
+  mainWindow.setBounds({ x: 300, y: 100 })
+
   /* 创建系统托盘 */
   tray = new Tray(path.join(__dirname, './image/e6.ico')) // 此写法为全局变量方便打包后使用
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: '显示', role: 'show', click: () => {
-        mainWindow.show()
-      }
+      label: '重启',
+        click() {
+          app.relaunch();
+          app.quit();
+        }
     },
     {
-      label: '退出', role: 'quit', click: () => {
-        app.quit()
-      }
+      label: '退出', 
+      role: 'quit', 
+        click: () => {
+          app.quit()
+        }
     } 
   ])
   tray.setToolTip(app.name)
@@ -37,13 +47,26 @@ app.whenReady().then(() => {
   })
 
   /* 创建快捷键 */
-  globalShortcut.register('Control+Space', () => {
+  globalShortcut.register('Alt+z', () => {
 
     mainWindow.show()
-    mainWindow.maximize()
+
+  })
+
+  /* 创建快捷键 */
+  globalShortcut.register('Control+Space', () => {
+
+    /* 创建项目窗口 */
+    const Window = new BrowserWindow({
+      icon : "./image/256x256.ico",
+      autoHideMenuBar : true
+    })
+
+    Window.show()
+    Window.maximize()
 
     /* 创建浏览文件夹 */
-    dialog.showOpenDialog(mainWindow, {
+    dialog.showOpenDialog(Window, {
         properties: ['openFile'],
         filters: [
             { name : 'All Files', extensions : ['*'] }
@@ -53,8 +76,8 @@ app.whenReady().then(() => {
             console.log('User manually canceled')
         } else {
             const filepath = result.filePaths[0]
-            mainWindow.loadURL(`file://${filepath}`) //该位置使用Tab键上面的符号
-            PDFWindow.addSupport(mainWindow)
+            Window.loadURL(`file://${filepath}`) //该位置使用Tab键上面的符号
+            PDFWindow.addSupport(Window)
         }
     }).catch(err => {
         console.log(err)
@@ -71,7 +94,7 @@ app.whenReady().then(() => {
 
     const win = new BrowserWindow({
       icon : "./image/256x256.ico",
-      autoHideMenuBar : true,
+      autoHideMenuBar : true
     })
 
     win.maximize()
